a7692b96ad056fed00dd155038fe2e1a3b63597b
BenjaminAmiel <benj34090@gmail.com>
2025-03-18 19:46:33 +0100

    Add files via upload

---
+++ Big_Bang.py
@@ -0,0 +1,10 @@
+ 
+# Big Bang - Algorithme Central
+# Créé par Benjamin Amiel
+
+def big_bang():
+    print("Bienvenue dans Big Bang - Algorithme Open Source")
+    print("L'IA rencontre la recherche fondamentale...")
+
+if __name__ == "__main__":
+    big_bang()
